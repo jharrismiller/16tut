566b2446271f6e5c82ac57a666a9ac4e33920518
jharrismiller <36432119+jharrismiller@users.noreply.github.com>
2025-02-11 20:25:44 -0500

    Skeleton

---
+++ src/NewPost.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewPost = () => {
+  return (
+    <main><h1>New Post</h1></main>
+  )
+}
+
+export default NewPost
---
+++ src/PostPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostPage = () => {
+  return (
+    <main><h1>Post Page</h1></main>
+  )
+}
+
+export default PostPage
